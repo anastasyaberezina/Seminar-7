1fb3808229bbb498a61f58b2d6ba89522f230a13
Anastasya <gavrilovaanastasya@yandex.ru>
2022-10-12 21:29:01 +0300

    Задать двум массив размена m на n. Каждый элемент находится по формуле Amn=m+n

---
+++ Seminar7.2/Program.cs
@@ -0,0 +1,38 @@
+﻿// Задать двум массив размена m на n. Каждый элемент находится по формуле Amn=m+n. Выведите полученный массив.
+
+Console.WriteLine("Количество строк: ");
+int rows = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Количество столбцов: ");
+int columns = int.Parse(Console.ReadLine()!);
+
+
+int[,] array = GetArray(rows, columns);
+PrintArray(array);
+
+
+
+int[,] GetArray(int m, int n)
+{
+    int[,] result = new int[m, n];
+    for(int i=0; i<m; i++)
+    {
+        for(int j=0; j<n; j++)
+        {
+            result[i,j]=i+j;
+        }
+    }
+    return result;
+}
+
+void PrintArray (int[,] array)
+{
+    for(int i=0; i<array.GetLength(0); i++)
+    {
+        for(int j=0; j<array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i,j]} ");
+        }
+        Console.WriteLine();
+    }
+}
