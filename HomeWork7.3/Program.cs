5d3760c26a705890a823ccfbc3fb13a21d1956ad
Anastasya <gavrilovaanastasya@yandex.ru>
2022-10-14 13:13:23 +0300

    Найдите среднее арифметическое элементов в каждом столбце двум массива

--- HomeWork7.3/Program.cs
+++ HomeWork7.3/Program.cs
@@ -1,22 +1,27 @@
 ﻿// Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+//Та же самая ошибка, не понимаю что не так.
 
-int rows = new Random().Next(5, 5);
-int columns = new Random().Next(5, 5);
+int rows = new Random().Next(5, 5); //Назначили кол-во строк и элементов в них
+int columns = new Random().Next(5, 5); //Назначили кол-во столбцов и элементов в них
 
-int[,] array = GetArray(rows, columns, 0, 10);
+int[,] array = GetArray(rows, columns, 0, 20); //Применили метод к новому пустому массиву, тем самым заполнив его случ числами от 0 до 20
 PrintArray(array);
-Console.WriteLine();
-int[,] Res = Arif(array);
-PrintArray(Res);
+Console.WriteLine(); //Отступ для красоты 
 
-int[,] Arif(int[,] arra, int row, int column)
+int Res = Arif(array); //Новый массив с результатом вычислений
+PrintArray(Res); 
+
+int Arif(int[,] arra) //Метод возвращает , принимает массив как аргумент
 {
-    int arifm = 0;
+    int arifm = 0; //Переменная искомое
     for(int i=0; i<arra.GetLength(0); i++)
     {
         for(int j=0; j<arra.GetLength(1); j++)
         {
-              arifm+=array[i,j];
+            if(i==0)
+              {
+                arifm+=array[i,j]/j;//Вычисляем среднее арифметическое
+              }
         }
     }
     return arifm;
