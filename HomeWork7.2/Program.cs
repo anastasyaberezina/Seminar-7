c61cfafc6440b291a4a3ab2bbe24e9a89aa8f4a2
Anastasya <gavrilovaanastasya@yandex.ru>
2022-10-14 12:48:50 +0300

    на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.

--- HomeWork7.2/Program.cs
+++ HomeWork7.2/Program.cs
@@ -1,38 +1,43 @@
 ﻿// Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
+//Мне кажется, все правильно делаю, но не работает.
 
-
-Console.WriteLine("Введите позицию элемента ");
-int position = int.Parse(Console.ReadLine()!);
 
 int rows = new Random().Next(5, 5);
 int columns = new Random().Next(5, 5);
 
-int[,] array = GetArray(rows, columns, 0, 10);
-PrintArray(array);
-int element = ElementValue(array);
-Console.WriteLine(element);
+int[,] array = GetArray(rows, columns, 0, 9);//Заполнили массив
+PrintArray(array);//показали его
 
-int ElementValue(int[,] array, int pos)
-{
-    int pos1=pos/10;
-    int pos2=pos%10;
-    int result = array[pos1,pos2];
+Console.WriteLine("Введите позицию элемента ");
+int position1 = int.Parse(Console.ReadLine()!);
 
-    for(int i=0; i<array.GetLength(0); i++)
+Console.WriteLine("Введите позицию элемента ");
+int position2 = int.Parse(Console.ReadLine()!);
+
+//int arrayposition = array[position1,position2];//Заданный элемент
+
+int element = ElementArray(array);
+//Console.WriteLine(element);
+
+int ElementArray(int[,] arra, int row, int column, int pos1, int pos2)
+{ 
+    int[,] result = new int[row, column];//Назначили пустому массиву наш массив
+    int position = 0; //Обозначили искомый элемент и присвоили значения
+    for(int i=0; i<row; i++)
     {
-        for(int j=0; j<array.GetLength(1); j++)
+        for(int j=0; j<column; j++)
         {
-            if((i==pos1&&j==pos2))
+            if((i==pos1)&&(j==pos2))
             {
-              result=array[i,j];
+              position=result[i,j]; //Присвоили значения одного элемента другому
             }
             else
             {
                 Console.WriteLine("Такой элемент отсутствует в массиве");
             }
+             return position;
         }
     }
-    return result;
 }
 
 int[,] GetArray(int rows, int columns, int minValue, int maxValue) //Функция заполнения массива.
